4cb64bb322688db2c58fbbeb8f2f92a51f89bddb
Rafael Rodrigues <rafael7rodrigues@gmail.com>
2019-03-18 21:37:41 -0300

    Configurando server para utilizar HTTPS, Typescript e restartar automaticamente

---
+++ backend/dist/server.js
@@ -0,0 +1,21 @@
+"use strict";
+exports.__esModule = true;
+var jsonServer = require("json-server");
+// Módulo que é capaz de ler os arquivos no disco (utilizar certificado e a chave)
+var fs = require("fs");
+var https = require("https");
+// Tipar constante "server" com objeto "express", assim fica possível fazer autocomplete dos métodos disponíveis em uma
+// aplicação express e tratar melhor os erros
+var server = jsonServer.create();
+var router = jsonServer.router('db.json');
+var middlewares = jsonServer.defaults();
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
+server.use(router);
+var options = {
+    cert: fs.readFileSync('./backend/keys/cert.pem'),
+    key: fs.readFileSync('./backend/keys/key.pem')
+};
+https.createServer(options, server).listen(3001, function () {
+    console.log('JSON Server is running on https://localhost:3001');
+});
---
+++ backend/server.ts
@@ -0,0 +1,32 @@
+import * as jsonServer from 'json-server'
+
+// Importar o "Express", que é o tipo que representa uma aplicação "Express", p facilitar na tipação, fazer autocomplete...
+import {Express} from 'express'
+
+// Módulo que é capaz de ler os arquivos no disco (utilizar certificado e a chave)
+import * as fs from 'fs'
+import * as https from 'https'
+
+// Tipar constante "server" com objeto "express", assim fica possível fazer autocomplete dos métodos disponíveis em uma
+// aplicação express e tratar melhor os erros
+const server: Express = jsonServer.create()
+
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+
+
+server.use(middlewares)
+
+server.use(jsonServer.bodyParser)
+
+server.use(router)
+
+
+const options = {
+    cert: fs.readFileSync('./backend/keys/cert.pem'),
+    key: fs.readFileSync('./backend/keys/key.pem')
+}
+
+https.createServer(options, server).listen(3001, () => {
+    console.log('JSON Server is running on https://localhost:3001')
+})
